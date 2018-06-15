3f58da80f57202bb863de70bfd4955ee9f129237
Ben Holmes <ben@coolship.io>
2018-06-15 15:33:32 -0400

    Updated demo.js.

--- pages/demo.js
+++ pages/demo.js
@@ -4,11 +4,15 @@ import Triangle from '../components/triangle.js'
 export default () => (
     <Layout>
        <p>This is the demo page</p>
+
+       <Regl width={window.innerWidth}
+       height={window.innerHeight}
+       onFrame={this.onFrameHandler.bind(this)}>
+
+       <Triangle color={[1,1,0.5,1]}
+       positions={[[-0.5, 0],[0, -0.5],[0.25, 1]]} />
+       </Regl >
+
     </Layout>
-    <Regl width={window.innerWidth}
-              height={window.innerHeight}
-              onFrame={this.onFrameHandler.bind(this)}>
-              <Triangle color={[1,1,0.5,1]}
-                        positions={[[-0.5, 0],[0, -0.5],[0.25, 1]]} />
-        </Regl >
+
 )

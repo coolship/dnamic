bfe46eae6dca20f080bc5168e201c3dd946e99c2
Ben Holmes <ben@coolship.io>
2018-06-15 14:12:50 -0400

    tweaks

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Nav from './nav'
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+}
+
+const Layout = (props) => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
+)
+
+export default Layout

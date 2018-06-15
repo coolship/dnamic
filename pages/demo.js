f5e1cc8e29a8438baea8cb3bef550fd986513825
Ben Holmes <ben@coolship.io>
2018-06-15 15:26:04 -0400

    Updated demo.js.

--- pages/demo.js
+++ pages/demo.js
@@ -5,6 +5,10 @@ export default () => (
     <Layout>
        <p>This is the demo page</p>
     </Layout>
-    <Triangle>
-    </Triangle>
+    <Regl width={window.innerWidth}
+              height={window.innerHeight}
+              onFrame={this.onFrameHandler.bind(this)}>
+              <Triangle color={[1,1,0.5,1]}
+                        positions={[[-0.5, 0],[0, -0.5],[0.25, 1]]} />
+        </Regl >
 )

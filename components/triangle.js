594b4a8ef94dd77552052d8ed831e6f80449f894
Ben Holmes <ben@coolship.io>
2018-06-15 15:26:40 -0400

    Updated index.js.

---
+++ components/triangle.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Draw } from 'react-regl';
+
+class Triangle extends Component {
+  render(){
+    const {positions, color} = this.props;
+
+    return (
+      <Draw vert={triVert}
+            frag={triFrag}
+            attributes={{
+              positions: this.props.positions
+            }}
+            uniforms={{
+              color: color this.props.color
+            }}
+            count={3}
+      />
+    );
+  }
+}
